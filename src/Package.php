d0219819fac39aa45dcf49ce2b74b0e589f3c546
Adrian Brown <adrianrb93@gmail.com>
2022-06-12 02:18:35 +1000

    Lambda doesn't like the testbench bootstrap.

--- src/Package.php
+++ src/Package.php
@@ -36,6 +36,14 @@ class Package extends Base
                 __DIR__ => 'src',
                 __DIR__ . '/../tests' => 'tests',
                 __DIR__ . '/Runtime' => '',
+            ])
+            ->includeStrings([
+                'bootstrap/app.php' => '<?php
+                    $app = new Illuminate\Foundation\Application($_ENV["APP_BASE_PATH"] ?? dirname(__DIR__));
+                    $app->singleton(Illuminate\Contracts\Http\Kernel::class, App\Http\Kernel::class);
+                    $app->singleton(Illuminate\Contracts\Console\Kernel::class, App\Console\Kernel::class);
+                    $app->singleton(Illuminate\Contracts\Debug\ExceptionHandler::class, App\Exceptions\Handler::class);
+                    return $app;',
             ]);
     }
 
