a1a50b892224f36d9f822a9abbd33ecc029a2261
Adrian Brown <adrianrb93@gmail.com>
2022-06-12 09:08:09 +1000

    Fix bootstrap kernels

--- src/Package.php
+++ src/Package.php
@@ -40,9 +40,9 @@ class Package extends Base
             ->includeStrings([
                 'bootstrap/app.php' => '<?php
                     $app = new Illuminate\Foundation\Application($_ENV["APP_BASE_PATH"] ?? dirname(__DIR__));
-                    $app->singleton(Illuminate\Contracts\Http\Kernel::class, App\Http\Kernel::class);
-                    $app->singleton(Illuminate\Contracts\Console\Kernel::class, App\Console\Kernel::class);
-                    $app->singleton(Illuminate\Contracts\Debug\ExceptionHandler::class, App\Exceptions\Handler::class);
+                    $app->singleton(Illuminate\Contracts\Http\Kernel::class, Illuminate\Foundation\Http\Kernel::class);
+                    $app->singleton(Illuminate\Contracts\Console\Kernel::class, Illuminate\Foundation\Console\Kernel::class);
+                    $app->singleton(Illuminate\Contracts\Debug\ExceptionHandler::class, Illuminate\Foundation\Exceptions\Handler::class);
                     return $app;',
             ]);
     }
